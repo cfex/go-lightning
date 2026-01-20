2908c11407821f05ef37dd8b24bae3c30cff1475
ddux <dusan.stanojevic.cs@gmail.com>
2026-01-20 11:17:44 -0600

    docs redone

--- lit/examples/usercrud/models/user.model.go
+++ lit/examples/usercrud/models/user.model.go
@@ -4,9 +4,9 @@ package models
 PostgreSQL:
 	CREATE TABLE IF NOT EXISTS users (
 		id SERIAL PRIMARY KEY,
-		first_name VARCHAR(255) NOT NULL,
-		last_name VARCHAR(255) NOT NULL,
-		email VARCHAR(255) UNIQUE NOT NULL
+		first_name TEXT NOT NULL,
+		last_name TEXT NOT NULL,
+		email TEXT UNIQUE NOT NULL
 	);
 */
 
